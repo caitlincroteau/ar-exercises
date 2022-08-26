04b6ba7de68cbe82a8f1577876552ccafa3b73bd
Caitlin <cjcroteau@gmail.com>
2022-08-26 21:38:02 +0000

    ex1. add store count

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -12,3 +12,6 @@ puts "----------"
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "number of stores is:"
+puts Store.count
