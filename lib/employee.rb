a981318ccd552c993e0c885eda33ec57520ea5a3
Caitlin <cjcroteau@gmail.com>
2022-08-27 01:50:16 +0000

    ex7. add validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: { message: "must be given please" }
+  validates :last_name, presence: { message: "must be given please" }
+  validates :hourly_rate, numericality: { only_integer: true, in: 40..200, message: "must be an integer between 40 and 200" }
+  validates :store_id, presence: { message: "must be given please" }
 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3, message: "must be at least 3 characters long" }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "must be an integer greater than 0" }
+  validate :must_carry_mens_or_womens_clothing, on: :create
+
+  def must_carry_mens_or_womens_clothing
+    errors.add(:id, "cannot be added. Store must carry either mens_apparel or womens_apparel or both.") unless
+    mens_apparel.present? || womens_apparel.present?
+  end
+
 
 end
